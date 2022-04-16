bc328924a5f05584f02461ae85f62baa8fe04f29
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2022-04-16 23:31:13 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Nitin Kumar.", "Web Designer.", "Web Developer.", "Freelancer.", "Ethical Hacker.", "Pentester."],
+      strings: ["Nitin Kumar.", "Python Lover.", "Automation Engineer.", "Hacktivist.", "Web Developer.", "Freelancer.", "Pentester."],
       typeSpeed: 100,
       loop: true,
     });
